8975eddd61ece109f14dc7e1c8d20dd5547381dc
Yuri Solovev <yura_sol@inbox.ru>
2018-10-07 11:57:37 +0300

    settings update

--- djilogreader/settings/prod.py
+++ djilogreader/settings/prod.py
@@ -3,7 +3,7 @@ from djilogreader.settings.base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['18.223.162.25', 'ec2-18-223-162-25.us-east-2.compute.amazonaws.com']
 
 SECRET_KEY = get_env_variable('SECRET_KEY')
 
@@ -14,7 +14,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': get_env_variable('DATABASE'),
-        'USER': get_env_variable('USER'),
+        'USER': get_env_variable('DB_USER'),
         'PASSWORD': get_env_variable('PASSWORD'),
         'HOST': get_env_variable('HOST'),
         'PORT': '5432',
@@ -24,11 +24,10 @@ DATABASES = {
 
 # Static files
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/home/ubuntu/static/'
 
 
 # Media files
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'profiles')
-MEDIA_URL = '/profiles/'
-
+MEDIA_ROOT = '/home/ubuntu/media/profiles/'
+MEDIA_URL = 'http://ec2-18-223-162-25.us-east-2.compute.amazonaws.com/media/profiles/'
